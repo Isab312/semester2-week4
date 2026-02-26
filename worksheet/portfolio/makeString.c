e1bfe2828112c606a3a5cb26836ecb82b9cdf2cf
Isab312 <qggg0310@leeds.ac.uk>
2026-02-26 15:02:31 +0000

    done week4  hwk

--- worksheet/portfolio/makeString.c
+++ worksheet/portfolio/makeString.c
@@ -7,13 +7,17 @@ Student ID:
 #include <stdio.h>
 #include <string.h>
 
-int main( int argc, char **argv ) {
+int main(int argc, char **argv) {
+    char buffer[50];  // holds the final string of watever we want
+    buffer[0] = '\0'; // initialise empty
 
-    // define appropriate data to hold your answer
+    for(int i = 1; i < argc; i++) {
+        strcat(buffer, argv[i]);         // add the argument on a counter
+        if(i < argc - 1) {
+            strcat(buffer, "-");        // add dash if not last argument
+        }
+    }
 
-    // process the command-line data using appropriate string functions
-
-    printf("%s\n",buffer); // use only this single print statement in your submitted code
-
+    printf("%s\n", buffer); // only print once
     return 0;
 }
